8b62cf72bf0785976f6f226829181aca674e1cdf
312134534 <ww2815953@gmail.com>
2025-09-25 23:55:23 +0800

    add template

--- codes/Dynamic Programming/LIS.cpp
+++ codes/Dynamic Programming/LIS.cpp
@@ -8,3 +8,22 @@ int LIS_length(const vector<int>& a) {
     }
     return (int)d.size();
 }
+// LIS + 找出實際序列
+for(int i=0;i<num.size();i++){
+    if(lis.empty()||lis.back()<num[i]){
+        lis.push_back(num[i]);
+        dp[i]=lis.size();
+    }
+    else{
+        auto iter=lower_bound(all(lis),num[i]);
+        dp[i]=iter-lis.begin()+1;
+        *iter=num[i];
+    }
+}
+int length=lis.size();
+for(int i=num.size()-1;i>=0;i--){
+    if(dp[i]==length){
+        ans.push_back(num[i]);
+        length--;
+    }
+}
