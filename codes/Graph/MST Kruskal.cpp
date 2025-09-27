4f7de07298c7f44ac14e5f41548b414179842f9d
ddd <ddd@gmail.com>
2025-09-27 17:41:58 +0800

    2025NCPC Codebook Compact Version

--- codes/Graph/MST Kruskal.cpp
+++ codes/Graph/MST Kruskal.cpp
@@ -1,21 +1,7 @@
 vector<ll> djs, sz;
-ll find(ll u) {
-    if (u == djs[u]) return u;
-    return djs[u] = find(djs[u]);
-}
-void join(ll u, ll v) {
-    u = find(u);
-    v = find(v);
-    if (u == v) return;
-    if (sz[u] < sz[v]) swap(u, v);
-    djs[v] = u;
-    sz[u] += sz[v];
-}
-void init(ll n) {
-    djs.clear(); djs.resize(n + 1);
-    for (ll i = 1; i <= n; ++i) djs[i] = i;
-    sz.clear(); sz.resize(n + 1, 1);
-}
+ll find(ll u) {...}
+void join(ll u, ll v) {...}
+void init(ll n) {...}
 
 class Edge{
 public:
